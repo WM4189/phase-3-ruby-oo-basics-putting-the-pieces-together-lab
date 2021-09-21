7298dfdece6b85bc24a01ed0bd591faf599eb9b9
William Mosto <will.mosto@gmail.com>
2021-09-21 11:37:17 -0400

    commit tree 0d0d0b58a5dfc6bcd15228e0f14a9582d6962b81

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,27 @@
 
+class Book
+    attr_accessor :title
+    attr_accessor :author
+    attr_accessor :page_count
+    attr_accessor :genre
 
+    def initialize(title)
+        @title = title
+    end
+
+    def turn_page
+       puts "Flipping the page...wow, you read fast!"
+    end
+
+end
+
+book = Book.new("War and Peace")
+book.author = "Tolstoy"
+book.page_count = 1225
+book.genre = "Epic"
+book.turn_page
+
+
+
+
+# ruby lib/book.rb
